f9a9e896eaef8ef5656ae5a2cbd78d6a53ab25d2
Tim Whitcomb <tim.whitcomb@nrlmry.navy.mil>
2018-05-30 21:15:36 -0700

    Remove builtin Cylc instance.

--- cylc-7.6.0/dev/bin/stop-group-demo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for S in $(cylc scan -n groups | awk '{print $1}'); do
-  echo $S
-  cylc stop $S
-done
--- cylc-7.6.0/tests/remote/basic/bin/cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# remove installed remote passphrase
-set -e
-cylc check-triggering "$@"
-if [[ $CYLC_TEST_TASK_OWNER@$CYLC_TEST_TASK_HOST == $USER@localhost || \
-      $CYLC_TEST_TASK_OWNER@$CYLC_TEST_TASK_HOST == $USER@$(hostname) ]]; then
-    exit
-fi
-echo "Done"
